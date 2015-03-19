ebc9e5c972af02523fb303dd2bdfe79d22e2e2f0
RafaelBMelo <rafamelo89@gmail.com>
2015-03-19 15:37:00 -0300

    Update L1_Q3.c

--- L1_Q3.c
+++ L1_Q3.c
@@ -1,1 +1,17 @@
+//3­ Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
 
+#include <stdio.h>
+
+int mostra_numero(int Num)
+{
+	printf("Digite um numero:\n");
+	scanf("%d", &Num);	
+	printf("O numero informado foi %d.\n", Num);
+}
+
+int main()
+{
+	int N;
+	mostra_numero(N);
+return 0;
+}
